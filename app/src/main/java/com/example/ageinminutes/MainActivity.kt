f169fcf19d408457ec27a0220ab14835a7fe31b0
Pete St. Martin <peter.r.stmartin@gmail.com>
2021-05-31 10:49:37 -0500

    App displays age in minutes after selecting birthdate

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.DatePicker
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,11 +30,30 @@ class MainActivity : AppCompatActivity() {
 
         DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener{
-                    view, year, month, dayOfMonth ->
-                    Toast.makeText(this,"DatePicker works", Toast.LENGTH_LONG).show()
+                    view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                    Toast.makeText(this,"The chosen year is $selectedYear, the month is ${selectedMonth+1}, and the day is $selectedDayOfMonth"
+                            , Toast.LENGTH_LONG).show()
+
+                    val selectedDate = "${selectedMonth+1}/$selectedDayOfMonth/$selectedYear"
+
+                    tvSelectedDate.setText(selectedDate)
+
+                    val sdf = SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH)
+
+                    val theDate = sdf.parse(selectedDate)
+
+                    val selectedDateInMinutes = theDate!!.time / 60000
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+                    val currentDateInMinutes = currentDate!!.time / 60000
+
+                    val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                    tvSelectedDateInMinutes.setText(differenceInMinutes.toString())
                 }
                 ,year
                 ,month
-                ,day ).show()
+                ,day).show()
     }
 }
